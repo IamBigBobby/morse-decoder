ef494d6593340d1667d818b57a2e1fcbb250a0f0
Denis Svetleishii <denissvetleishii@outlook.com>
2022-08-08 20:41:02 +0400

    feat: complete js task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,54 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = '';
+    
+    const arrExpr = [];
+    for (let i = 0; i < expr.length; i+=10){
+        arrExpr.push(expr.slice(i, i + 10));
+    }
+    
+    const arrEachElement = [];
+    for (let element of arrExpr){
+        if (element == '**********'){
+            arrEachElement.push(' ');
+        }
+        else{
+            arrEachElement.push(element);
+        }
+    }
+
+    const arrEachElementMorse = [];
+    for (let element of arrEachElement){
+        if (element == ' '){
+            arrEachElementMorse.push(' ');
+        }
+        else{
+            element = element.replace(/10/gi, '.');
+            element = element.replace(/11/gi, '-');
+            element = element.replace(/00/gi, '')
+            arrEachElementMorse.push(element);
+        }
+    }
+
+    const finalStringResult = [];
+    for(let element of arrEachElementMorse){
+        for (key in MORSE_TABLE){
+            if (element == key){
+                finalStringResult.push(MORSE_TABLE[key]);
+            }
+            else if (element == ' '){
+                finalStringResult.push(' ');
+                break;
+            }
+        }
+    }
+    result = finalStringResult.join('');
+    return result;
 }
 
 module.exports = {
     decode
 }
+
+console.log(decode("0000000011001010101000000000100011101111**********0000111011000000001000000000100010111110**********00000011110000000010**********00000010100000001110**********000000001100101010100000000010**********00111010100000001011000010101000000000100000001111000000001000000011100000000011"))
